5fab6b18e1ffd88e9e7d2b88719b313be3857b1a
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-09-03 17:37:52 -0400

    Added documentation for the fatal_error function

--- include/error.h
+++ include/error.h
@@ -7,6 +7,11 @@ typedef enum error_code_e {
     ERROR_MEMORY_ALLOCATION_FAILURE
 } error_code_t;
 
+/** This function is analogous to the POSIX 'strerror' function, except the
+ *  errno variable is replaced by a caller-specified error_code argument. This
+ *  allows the error module to use internal linkage for all the error messages.
+ * 
+ */
 void fatal_error(error_code_t error_code);
 
 #endif // PROJECT_INCLUDES_ERROR_H
--- src/error.c
+++ src/error.c
@@ -1,6 +1,11 @@
 
 #include "mmgr.h"
 
+/** This function is analogous to the POSIX 'strerror' function, except the
+ *  errno variable is replaced by a caller-specified error_code argument. This
+ *  allows the error module to use internal linkage for all the error messages.
+ * 
+ */
 void fatal_error(error_code_t error_code) {
     fprintf(stderr, "[Error] %d\n", error_code);
     exit(EXIT_FAILURE);
