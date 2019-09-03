7e0ce9d3a61a799e941034738c15da4346f7690b
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-09-03 17:08:07 -0400

    Copied documentation for allocate_memory to mem.c source file

--- src/mem.c
+++ src/mem.c
@@ -1,6 +1,17 @@
 
 #include "mmgr.h"
 
+/** This function is a wrapper around the C standard library's 'malloc'
+ *  function; its purpose is to implicitly conduct the necessary error-checking
+ *  on the pointer returned by malloc to ensure the operation has been
+ *  successful.
+ * 
+ *  The function will exit with an error if the pointer returned by malloc is
+ *  invalid, so the caller may be assured that execution beyond the call to
+ *  allocate_memory will proceed if and only if the call to malloc returns
+ *  successfully.
+ * 
+ */
 void* allocate_memory(size_t size) {
     void* memory_region = malloc(size);
 
